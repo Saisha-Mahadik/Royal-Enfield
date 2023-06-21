592fba6578376b5ab6a1ff1611cee12b359f5902
Saisha-Mahadik <136926547+Saisha-Mahadik@users.noreply.github.com>
2023-06-21 15:45:40 +0530

    First Commit

---
+++ RE.js
@@ -0,0 +1,27 @@
+function a(){
+    document.getElementById("kk").src="./images/22.avif"
+}
+function a1(){
+    document.getElementById("kk").src="./images/interceptor-home-motorcycle.avif"
+}
+function b(){
+    document.getElementById("kk").src="./images/23.avif"
+}
+function c(){
+    document.getElementById("kk").src="./images/24.avif"
+}
+function d(){
+    document.getElementById("kk").src="./images/25.avif"
+}
+function e(){
+    document.getElementById("kk").src="./images/26.avif"
+}
+function f(){
+    document.getElementById("kk").src="./images/27.avif"
+}
+function g(){
+    document.getElementById("kk").src="./images/28.avif"
+}
+function h(){
+    document.getElementById("kk").src="./images/29.avif"
+}
