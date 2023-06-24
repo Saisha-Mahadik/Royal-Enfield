864d5dda4f3fd155c7d6671b71608ab1d70ca255
Saisha-Mahadik <136926547+Saisha-Mahadik@users.noreply.github.com>
2023-06-24 17:57:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,3 +25,63 @@ function g(){
 function h(){
     document.getElementById("kk").src="./images/29.avif"
 }
+
+function Rides1(){
+    document.getElementById("Rideimg").src="./Images/13.png"
+}
+function Rides2(){
+    document.getElementById("Rideimg").src="./Images/14.png"
+}
+function Rides3(){
+    document.getElementById("Rideimg").src="./Images/19.png"
+}
+function Rides4(){
+    document.getElementById("Rideimg").src="./Images/15.png"
+}
+function Rides5(){
+    document.getElementById("Rideimg").src="./Images/16.png"
+}
+function Rides6(){
+    document.getElementById("Rideimg").src="./Images/17.png"
+}
+function Rides7(){
+    document.getElementById("Rideimg").src="./Images/18.png"
+}
+
+function Apparel1(){
+    document.getElementById("Apparelimg").src="./Images/apparel1.png"
+}
+function Apparel2(){
+    document.getElementById("Apparelimg").src="./Images/apparel2.png"
+}
+function Apparel3(){
+    document.getElementById("Apparelimg").src="./Images/apparel3.png"
+}
+function Apparel4(){
+    document.getElementById("Apparelimg").src="./Images/apparel4.png"
+}
+function Apparel5(){
+    document.getElementById("Apparelimg").src="./Images/apparel5.png"
+}
+function Apparel6(){
+    document.getElementById("Apparelimg").src="./Images/apparel6.png"
+}
+
+function Accessories1(){
+    document.getElementById("Accessoriesimg").src="./Images/Accessories1.png"
+}
+function Accessories2(){
+    document.getElementById("Accessoriesimg").src="./Images/Accessories2.png"
+}
+
+function LocateUs1(){
+    document.getElementById("Locateimg").src="./Images/LocateUs1.png"
+}
+function LocateUs2(){
+    document.getElementById("Locateimg").src="./Images/LocateUs2.png"
+}
+function LocateUs3(){
+    document.getElementById("Locateimg").src="./Images/LocateUs3.png"
+}
+
+
